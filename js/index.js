a7a93b6d3483ea03be6aabe98a181228f1346a45
Juan Alcantara <juan@cordage.io>
2022-06-27 15:31:13 -0500

    Github pages

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 import currentWeather from './current-weather.js'
+import { ViewportSize } from './utils/viewport.js'
+
+const $app = document.querySelector('#app')
+const $loader = document.querySelector('#loading')
+
+ViewportSize($app)
+ViewportSize($loader)
 
 currentWeather()
